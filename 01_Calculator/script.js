058df943d999d965e68a8c5cebba5b87400c0259
Bhabani Barik <bhabanishankar.barik21@gmail.com>
2024-02-21 21:39:11 +0530

    update script.js

---
+++ 01_Calculator/script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButton = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelectorAll('[data-equals]');
+const deleteButton = document.querySelectorAll('[data-delete]');
+const allClearButton = document.querySelectorAll('[data-all-clear]');
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand');
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
